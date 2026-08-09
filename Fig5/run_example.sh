922a575d42239eb879245b778e0931d74a028fa3
jylee07 <ghsl0102@korea.ac.kr>
2026-08-09 21:02:05 +0900

    Delete Fig5 directory

--- Fig5/run_example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-Rscript figure5_cfgjkl.R \
-  --kmaster Figure5_KMASTER_modeling_matrix.tsv \
-  --genie Figure5_GENIE_validation_matrix.tsv \
-  --outdir results \
-  --seed 42 \
-  --stability_iterations 100
