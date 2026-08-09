609af4438831a602c806a4aee1c320007a71ff7e
jylee07 <ghsl0102@korea.ac.kr>
2026-08-09 21:01:57 +0900

    Delete Fig4 directory

--- Fig4/run_example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-Rscript figure4_aef.R \
-  --input KM00_Figure4_binary_feature_matrix.tsv \
-  --outdir results \
-  --iterations 100 \
-  --seed 123 \
-  --min_cancer_n 30
